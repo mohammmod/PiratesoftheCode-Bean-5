1f00bda784466946e9405ebfaba8f143c6c999f0
mohammmod <mohammad.sawas2@outlook.com>
2018-04-27 01:07:31 +0200

    initilize my project

---
+++ code5/src/main/java/at/refugeescode/code5/Code5Application.java
@@ -0,0 +1,12 @@
+package at.refugeescode.code5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Code5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Code5Application.class, args);
+	}
+}
